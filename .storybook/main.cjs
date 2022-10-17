697c05a3ded114a77feeaf9f65523843c9cfd78e
Alex Faustino <faustino.alex@outlook.com>
2022-10-17 12:11:42 -0300

    chore: update config type

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -13,7 +13,7 @@ module.exports = {
     storyStoreV7: true,
   },
   viteFinal: (config, { configType }) => {
-    if (configType == "PRODUCTION") {
+    if (configType === "PRODUCTION") {
       config.base = "/design-system-lab/";
     }
 
